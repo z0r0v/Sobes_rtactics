0ed125dba5f7749c46e3bf0c649b34f882e0e3d1
Your Name <3767082@mail.ru>
2020-07-21 15:58:26 +0300

    init project

---
+++ 01_type.js
@@ -0,0 +1,62 @@
+// const getTypeOf = (elem)=> {
+//   return   typeof elem;
+// };
+// console.log(getTypeOf(0)); //number
+// console.log(getTypeOf(true)); //boolean
+// console.log(getTypeOf("JavaScript")); //string
+// console.log(getTypeOf(undefined)); //undefined
+// console.log(getTypeOf(Math)); //object
+// console.log(getTypeOf(Symbol('JS'))); //symbol
+// console.log(getTypeOf(null)); //object
+// console.log(getTypeOf(function () {})); //function
+// console.log(getTypeOf(NaN)); //number
+//
+//
+// let lang = 'JS';
+// if (lang) {
+//   console.log('The best lang is' , lang); //The best lang is JS
+// }
+//
+// console.log(Boolean('')); //false
+// console.log(Boolean('Hello')); //true
+// console.log(Boolean(' '));//true
+// console.log(Boolean('0')); //true
+// console.log(Boolean(0)); //false
+// console.log(Boolean(null)); //false
+// console.log(Boolean([])); //true
+// console.log(Boolean({})); //true
+// console.log(Boolean(function () {})); //true
+//
+// console.log(1 + '2'); //10
+// console.log('' + 1 + 0); //12
+// console.log('' - 1 + 0); // -1
+// console.log('3' * 8); //24
+// console.log(4 + 10 + "px"); //14px
+// console.log('px' + 5 + 10); //px510
+// console.log('42' - 40); //2
+// console.log('42px' - 40); //NaN
+// console.log(null + 2); //2
+// console.log(undefined + 42); //NaN
+//
+// console.log(2 == "2"); //true
+// console.log(2 === '2'); //false
+// console.log(undefined == null); //true
+// console.log(undefined === null); //false
+//
+// console.log('0' == false); //true
+// console.log('0' == 0); //true
+// console.log(0 == 0); //true
+//
+//
+// console.log(false == ''); //true
+// console.log(false == []); //true
+// console.log(false == {}); //false
+// console.log('' == []); //true
+// console.log('' == {}); //false
+// console.log(0 == []); //true
+// console.log(0 == {}); //false
+// console.log(0 == null); //false
+//
+//
+//
+//
---
+++ 02_values.js
@@ -0,0 +1,12 @@
+// let a = 42;
+// let b = a;
+// b++;
+// console.log("a", a); //a 42
+// console.log("b", b); //b 43
+
+
+let a = [1, 2, 3];
+let b = a;
+b.push(4);
+console.log("a", a); //a [ 1, 2, 3, 4 ]
+console.log('b', b); //b [ 1, 2, 3, 4 ]
